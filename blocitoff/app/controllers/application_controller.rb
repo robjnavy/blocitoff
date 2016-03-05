a9251b17cb90dd4af6a7284365227d412f2f39e6
robjnavy <robjnavy@gmail.com>
2016-03-05 16:52:28 +0000

    sign up with css

--- blocitoff/app/controllers/application_controller.rb
+++ blocitoff/app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  
+  def layout
+
+			if !user_signed_in?
+				#return "devise_layout"
+				return "../devise/sessions/new"
+			else
+				return "application"
+			end
+		end
 end
